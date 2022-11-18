95cef0fead972c315eb5ced9e591ec2fbd6470b4
Manav Kapila <manav14kapila12@gmail.com>
2022-11-18 14:38:13 +0530

    initial commit- adding files to empty repo

---
+++ one/one.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    cout<<"\n ONE ";
+    return 0;
+}
---
+++ three/three.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    cout<<"\n THREE ";
+    return 0;
+}
